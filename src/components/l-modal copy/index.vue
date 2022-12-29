ab6186e25b4ae849dcc3c35486898abfc3cca506
lean.li <lean.li@marketingforce.com>
2022-12-29 19:45:51 +0800

    feat: 公共弹出框组件

---
+++ src/components/l-modal copy/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <m-modal
+    :visible="visible"
+    v-bind="{...defaultConfig, ...others}"
+    @ok="emits('handle:ok')"
+    @cancel="emits('handle:cancel')"
+  >
+    <template #title>{{ title }}</template>
+    <slot></slot>
+  </m-modal>
+</template>
+
+<script setup lang="ts">
+defineProps<{visible: boolean; title: string; others?: {[key: string]: any}}>();
+
+const defaultConfig = {
+  titleAlign: "start",
+  draggable: true,
+};
+const emits = defineEmits(["handle:ok", "handle:cancel"]);
+</script>
