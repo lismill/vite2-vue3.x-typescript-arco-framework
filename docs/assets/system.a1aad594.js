06f72ca1e41d8192b60ab29a9de7fe7cb619f40d
lean.li <lean.li@marketingforce.com>
2023-01-12 17:22:05 +0800

    docs: update

---
+++ docs/assets/system.a1aad594.js
@@ -0,0 +1,1 @@
+import{H as a}from"./index.a55774b5.js";const s=s=>a.get("/account/list",{params:s}),t=s=>a.post("/information/list",s);export{t as a,s as g};
--- docs/assets/system.e98f04a2.js
+++
@@ -1,1 +0,0 @@
-import{G as s}from"./index.2512bcfd.js";const t=t=>s.get("/account/list",{params:t}),a=t=>s.post("/information/list",t);export{a,t as g};
